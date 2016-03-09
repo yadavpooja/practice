0715bd488b1d8d86b44fa850f1223c16035befc1
Pooja Yadav <ypooja510@gmail.com>
2016-03-09 10:40:30 +0530

    Add number.py

---
+++ number.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+number = int(input("enter the number"))
+if number < 100:
+    print("number less than 100")
+elif number > 500:
+    print("number more than 500")
+else:
+    print("number less than 500")
